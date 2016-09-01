ee44fef0bdc4d896d7fdce10efaf59eff2abf1a3
Yuriy <borodatuu@gmail.com>
2016-09-01 15:20:02 +0000

    third

--- js-2.js
+++ js-2.js
@@ -2,14 +2,14 @@ var fibNum = Number(prompt('Enter number fibonacci:'));
 function fibonacci(n) {
     var t = [];
     var i;
-    t[0] = 1;
     t[1] = 1;
-    for (i = 2; i < n + 1; i++) {
+    t[2] = 1;
+    for (i = 3; i < n + 1; i++) {
         t[i] = t[i - 1] + t[i - 2];
     }
     document.write(t[n]);
     document.write('<br>');
-    for (i = 0; i < (n + 1); i++) {
+    for (i = 1; i < (n + 1); i++) {
         document.write(t[i] + ",");
     }
     
