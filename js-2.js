fcd361853dc44356d0e06933111f054c59015ab2
Yuriy <borodatuu@gmail.com>
2016-09-01 15:12:35 +0000

    second

--- js-2.js
+++ js-2.js
@@ -1,5 +1,5 @@
-var fabNum = Number(prompt('Enter number fabiachi:'));
-function fabiachi(n) {
+var fibNum = Number(prompt('Enter number fibonacci:'));
+function fibonacci(n) {
     var t = [];
     var i;
     t[0] = 1;
@@ -7,8 +7,11 @@ function fabiachi(n) {
     for (i = 2; i < n + 1; i++) {
         t[i] = t[i - 1] + t[i - 2];
     }
+    document.write(t[n]);
+    document.write('<br>');
     for (i = 0; i < (n + 1); i++) {
         document.write(t[i] + ",");
     }
+    
 }
-    fabiachi(fabNum);
+    fibonacci(fibNum);
